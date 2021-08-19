66b1531efbfec232f0c7e296db63c96defe7f4cc
Burak GÖK <79623899+gokburak@users.noreply.github.com>
2021-08-19 21:03:42 +0300

    redux

---
+++ src/reducers/index.js
@@ -0,0 +1,28 @@
+
+import { combineReducers } from "redux";
+import counterReducer from "./counterReducer";
+
+
+
+const reducers = combineReducers({
+    counterReducer
+});
+
+export default reducers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
